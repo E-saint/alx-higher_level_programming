8f4c41f786c25978dcc356ce4e78537fcdfbd27f
E-saint <esaintaic@gmail.com>
2023-10-12 07:08:16 -0700

    0x04. Python

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,7 +2,7 @@
 #include <Python.h>
 
 /**
- * print_python_bytes - Print bytes information
+ * print_python_bytes - Prints bytes information
  *
  * @p: Python Object
  * Return: no return
@@ -42,7 +42,7 @@ void print_python_bytes(PyObject *p)
 }
 
 /**
- * print_python_list - Print list information
+ * print_python_list - Prints list information
  *
  * @p: Python Object
  * Return: no return
